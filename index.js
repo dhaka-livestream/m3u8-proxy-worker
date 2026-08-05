60123a6316d6d0cae9afaec6ad6e1f141b7ddaa3
dhaka-livestream <146126511+dhaka-livestream@users.noreply.github.com>
2026-08-05 06:25:39 +0600

    Update Zee Bangla stream URL

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const CHANNEL_MAP = {
   "zeebanglahd-bdx2": "http://s3.itcnbd.live/server-4/stream/aHR0cDovLzE3Mi4xNi4yMDAuMjA1OjgwODgvMzAxL3RyYWNrcy12MWExL21vbm8ubTN1OD90b2tlbj00MTNmNjk2N2JjMWVkZmRkZDk2MzJmYzg4NmMwNjcyYTQ4ZDViZDgzLWI5NTkyYjI5OTAzMWZhNTUwMWQxNGJiYWZmN2NiNmI2LTE3ODU4NTMwOTctMTc4NTg0OTQ5Nw.m3u8",
   "starjalshahd-bdx3": "https://footfytv.pro/proxy/direct?url=http://103.151.61.12/Star_Jalsha/tracks-v1a1/mono.m3u8",
 "sonyaath-bdx2": "https://s3.itcnbd.live/server-4/stream/aHR0cDovLzE3Mi4xNi4yMDAuMjA1OjgwODgvMzA2L3RyYWNrcy12MWExL21vbm8ubTN1OD90b2tlbj02MzgwMWM2ODcyZWMyN2JlOTEyYjAxMTQzMjhlZTdmNWVhZGUyOWQxLThlMjI1YmFjMDM5ZjA4YmJmNzZiZmRkOTU5YzEwNDExLTE3ODU4ODkxNDUtMTc4NTg4NTU0NQ.m3u8",
-"zeebanglasd": "https://s2.itcnbd.live/server-2/stream/zee_bangla.m3u8",
+"zeebanglasd": "http://27.124.71.27/Zee_Bangla/index.m3u8",
 "sports": "https://another-server.com/sports/playlist.m3u8",
 "sports": "https://another-server.com/sports/playlist.m3u8",
 
